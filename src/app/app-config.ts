198c60e8214a7d00b2f9c47e9b46fa926262ed3c
gitcleber <cleber.san@yahoo.com>
2022-09-20 22:22:12 -0300

    update angular json erro budget

--- src/app/app-config.ts
+++ src/app/app-config.ts
@@ -1,20 +1,20 @@
 
 
 
-export default{
-  oidc: {
-    clientId: '0oa6fawe2p6XqcAI15d7',
-    issuer: 'https://dev-62087076.okta.com/oauth2/default',
-    redirectUri: 'https://spa-dev-class.vercel.app/login/callback',
-    scopes: ["openid", "profile", "email"],
-    testing: {
-      disableHttpsCheck: true,
-    },
-  },
-  widget: {
-    useInteractionCodeFlow: true,
-  },
-};
+// export default{
+//   oidc: {
+//     clientId: '0oa6fawe2p6XqcAI15d7',
+//     issuer: 'https://dev-62087076.okta.com/oauth2/default',
+//     redirectUri: 'https://spa-dev-class.vercel.app/login/callback',
+//     scopes: ["openid", "profile", "email"],
+//     testing: {
+//       disableHttpsCheck: true,
+//     },
+//   },
+//   widget: {
+//     useInteractionCodeFlow: true,
+//   },
+// };
 // export default{
 //   oidc: {
 //     clientId: '0oa6fawe2p6XqcAI15d7',
