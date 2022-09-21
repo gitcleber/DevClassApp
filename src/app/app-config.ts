bfdad82e6952fd1948ece0ee6b98c03f06d0a1d1
gitcleber <cleber.san@yahoo.com>
2022-09-20 21:08:54 -0300

    update app-conf for prod

--- src/app/app-config.ts
+++ src/app/app-config.ts
@@ -1,25 +1,11 @@
 
 
 
-// export default{
-//   oidc: {
-//     clientId: '0oa6fawe2p6XqcAI15d7',
-//     issuer: 'https://dev-62087076.okta.com/oauth2/default',
-//     redirectUri: 'https://spa-dev-class.vercel.app/login/callback',
-//     scopes: ["openid", "profile", "email"],
-//     testing: {
-//       disableHttpsCheck: true,
-//     },
-//   },
-//   widget: {
-//     useInteractionCodeFlow: true,
-//   },
-// };
 export default{
   oidc: {
     clientId: '0oa6fawe2p6XqcAI15d7',
     issuer: 'https://dev-62087076.okta.com/oauth2/default',
-    redirectUri: 'http://localhost:4200/login/callback',
+    redirectUri: 'https://spa-dev-class.vercel.app/login/callback',
     scopes: ["openid", "profile", "email"],
     testing: {
       disableHttpsCheck: true,
@@ -29,6 +15,20 @@ export default{
     useInteractionCodeFlow: true,
   },
 };
+// export default{
+//   oidc: {
+//     clientId: '0oa6fawe2p6XqcAI15d7',
+//     issuer: 'https://dev-62087076.okta.com/oauth2/default',
+//     redirectUri: 'http://localhost:4200/login/callback',
+//     scopes: ["openid", "profile", "email"],
+//     testing: {
+//       disableHttpsCheck: true,
+//     },
+//   },
+//   widget: {
+//     useInteractionCodeFlow: true,
+//   },
+// };
 
 // export const oktaConfig = {
 //     clientId: '0oa6fawe2p6XqcAI15d7',
